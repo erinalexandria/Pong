d11c0c0b82c00bfa251f06e23a8c62e16b235d2b
Erin Brown <brown.erin325@gmail.com>
2023-12-27 11:22:56 -0500

    created main game loop

---
+++ pong.py
@@ -0,0 +1,13 @@
+import turtle
+
+#create a window
+wn = turtle.Screen()
+wn.title("Pong by Erin")
+wn.bgcolor("black")
+wn.setup(width=800, heigh=600)
+#tracer stops the window from updating
+wn.tracer(0)
+
+#main game loop
+while True:
+    wn.update()
