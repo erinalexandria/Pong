e1cd77d8a012759c0bc0ed1d4cc49461115d5351
Erin Brown <brown.erin325@gmail.com>
2023-12-27 11:33:42 -0500

    added second paddle

--- pong.py
+++ pong.py
@@ -13,11 +13,18 @@ paddle_a = turtle.Turtle()
 paddle_a.speed(0) #speed of animation
 paddle_a.shape("square")
 paddle_a.color("white")
-paddle_a.shapesize(stretttch_wid=5, stretch_len=1)
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup() #no line drawn
 paddle_a.goto(-350, 0)
 
 #Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0) #speed of animation
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup() #no line drawn
+paddle_b.goto(350, 0)
 
 #Ball
 
