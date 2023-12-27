e2fcfecf8a26d8737740e4e1ed0253ca77426df5
Erin Brown <brown.erin325@gmail.com>
2023-12-27 11:32:31 -0500

    paddle update

--- pong.py
+++ pong.py
@@ -8,6 +8,19 @@ wn.setup(width=800, height=600)
 #tracer stops the window from updating
 wn.tracer(0)
 
+#Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0) #speed of animation
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretttch_wid=5, stretch_len=1)
+paddle_a.penup() #no line drawn
+paddle_a.goto(-350, 0)
+
+#Paddle B
+
+#Ball
+
 #main game loop
 while True:
     wn.update()
